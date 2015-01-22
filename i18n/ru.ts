8344046871c3d92b05bce7d15dc8ee5a8d67e5b3
Chupligin Sergey <neochapay@gmail.com>
2015-01-22 21:20:15 +0300

    Обновил перевод

--- i18n/ru.ts
+++ i18n/ru.ts
@@ -1,11 +1,34 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>CurrentShow</name>
     <message>
         <source>Linear Air</source>
         <translation type="unfinished">Линейный Эфир</translation>
+    </message>
+</context>
+<context>
+    <name>NavigationPanel</name>
+    <message>
+        <source>Playlist</source>
+        <translation type="unfinished">Плейлист</translation>
+    </message>
+    <message>
+        <source>Library</source>
+        <translation type="unfinished">Библиотека</translation>
+    </message>
+    <message>
+        <source>Calendar</source>
+        <translation type="unfinished">Календарь</translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation type="unfinished">Информация</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished">Настройки</translation>
     </message>
 </context>
 <context>
@@ -16,11 +39,11 @@
     </message>
     <message>
         <source>Previews:</source>
-        <translation type="unfinished">Превидущий</translation>
+        <translation type="unfinished">Превидущий:</translation>
     </message>
     <message>
         <source>Next:</source>
-        <translation type="unfinished">Следующий</translation>
+        <translation type="unfinished">Следующий:</translation>
     </message>
 </context>
 <context>
